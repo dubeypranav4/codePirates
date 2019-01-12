18251f96954b60838b5e72991daed5775ebb9f2b
Pranav Dubey <dubeypranav4@gmail.com>
2019-01-12 20:18:22 +0530

    couchbase added

--- src/main/java/Zomato/controller/TestController.java
+++ src/main/java/Zomato/controller/TestController.java
@@ -1,11 +1,17 @@
 package Zomato.controller;
 
+import Zomato.service.couchbase.CouchBaseDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
+
+
+    @Autowired
+    CouchBaseDAO couchBaseDAO;
 
     @ResponseBody
     @RequestMapping(value = "test", method = RequestMethod.GET)
@@ -16,6 +22,11 @@ public class TestController {
         return "done";
     }
 
+    @ResponseBody
+    @RequestMapping(value = "checkCouch",method = RequestMethod.GET)
+    public String getDiagnosis(){
+        return couchBaseDAO.diagnos();
+    }
 
 
 }
--- src/main/java/Zomato/service/couchbase/CouchBaseDAO.java
+++ src/main/java/Zomato/service/couchbase/CouchBaseDAO.java
@@ -1,4 +1,84 @@
 package Zomato.service.couchbase;
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.CouchbaseCluster;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
 public class CouchBaseDAO {
+
+	@Value("${couchbase.cluster}")
+	private  String clusterName;// = "0.0.0.0:8091";
+
+	@Value("${couchbase.cluster.username}")
+	private String username;// = "admin";
+
+	@Value("${couchbase.cluster.password}")
+	private String password;// = "codepir";
+
+	@Value("${couchbase.default.bucket}")
+	private String bucketName;// = "TestBucket";
+
+	private Cluster cluster = null;
+
+	private Map<String, Bucket> buckets = null;
+
+//	private static Logger logger = LogManager.getRootLogger();
+	@PostConstruct
+	private void init(){
+//		logger.log(Level.INFO,"Trying to connect to couchbase cluster : " + clusterName);
+		System.out.println(username + " " + clusterName + " " + password);
+		cluster = CouchbaseCluster.create(clusterName);
+		buckets = new HashMap<>();
+		cluster.authenticate(username,password);
+		openBucket(bucketName);
+//		logger.log(Level.INFO,"Successfully connected to couchbase cluster :" + clusterName);
+//		logger.log(Level.INFO,cluster.diagnostics().toString());
+
+	}
+
+
+	public String diagnos(){
+		return cluster.diagnostics().toString();
+	}
+
+	private Bucket openBucket(String bucketName) {
+		if(cluster == null){
+//			logger.log(Level.INFO,"cluster not initialized ... run init");
+			return null;
+		}
+
+		if (buckets.containsKey(bucketName)){
+			return buckets.get(bucketName);
+		}
+		Bucket bucket = null;
+		try {
+			bucket = cluster.openBucket(bucketName);
+		}catch (Exception e){
+//			logger.log(Level.ERROR,e);
+			return null;
+		}
+
+		buckets.put(bucketName,bucket);
+		return bucket;
+	}
+
+
+
+	@PreDestroy
+	private void free(){
+//		buckets.entrySet().stream().forEach(t -> {t.getValue().close();logger.log(Level.INFO,"bucket " + t.getKey() + " closed");});
+		buckets = null;
+		cluster.disconnect();
+		cluster = null;
+//		logger.info("Cluster " + clusterName + " disconnected");
+	}
 }
