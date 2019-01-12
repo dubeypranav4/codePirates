b0f8376f56ed45bc697b30944e4933a487c431c9
yash.chhabra@yatra.com <yash.chhabra@yatra.com>
2019-01-12 17:29:08 +0530

    commit

---
+++ src/main/java/Zomato/controller/TestController.java
@@ -0,0 +1,26 @@
+package Zomato.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @ResponseBody
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    public String getPricingSessionResponseXml() {
+
+        String response = "";
+        Logger logger = LogManager.getRootLogger();
+        logger.info("logger working fine");
+
+        return "done";
+    }
+
+
+
+}
