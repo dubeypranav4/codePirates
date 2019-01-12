1e1100c7ef913c1742f6d9f9d1a518f5e8187778
yash.chhabra@yatra.com <yash.chhabra@yatra.com>
2019-01-12 18:21:18 +0530

    pom

--- src/main/java/Zomato/controller/TestController.java
+++ src/main/java/Zomato/controller/TestController.java
@@ -1,12 +1,10 @@
 package Zomato.controller;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 @Controller
 public class TestController {
 
