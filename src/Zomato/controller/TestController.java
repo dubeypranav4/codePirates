1cf59a3fa66c16c893b1a66a6d6c33ac1aa55e51
yash.chhabra@yatra.com <yash.chhabra@yatra.com>
2019-01-12 15:45:30 +0530

    init structure
    0.0

---
+++ src/Zomato/controller/TestController.java
@@ -0,0 +1,11 @@
+package Zomato.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("test")
+public class TestController {
+
+
+}
